72850343a7db07dfaedc997257aaf68889e18649
yinbp <yin-bp@163.com>
2020-04-25 20:41:57 +0800

    # v6.1.0 功能改进 1. 数据同步工具改进
      如果在程序里面配置的增量字段类型发生改变，要把增量状态表对应的类型调整为最新的字段类型
    
      设置了类型后，就按照类型来，不再按照设置的日期和数字字段名称来区分：
       
      增加setLastValueColumn方法，废弃setDateLastValueColumn和setNumberLastValueColumn两个方法
    2. DB-DB数据同步工具：增加在作业中直接指定sql语句的功能
    3. 修复数据同步bug：数据库到数据库跨库同步无效

--- src/main/java/org/frameworkset/elasticsearch/imp/ES2ESScrollAllTimestampDemo.java
+++ src/main/java/org/frameworkset/elasticsearch/imp/ES2ESScrollAllTimestampDemo.java
@@ -56,7 +56,7 @@ public class ES2ESScrollAllTimestampDemo {
 		//指定导入数据的sql语句，必填项，可以设置自己的提取逻辑，
 		// 设置增量变量log_id，增量变量名称#[log_id]可以多次出现在sql语句的不同位置中，例如：
 		// select * from td_sm_log where log_id > #[log_id] and parent_id = #[log_id]
-		// log_id和数据库对应的字段一致,就不需要设置setNumberLastValueColumn和setNumberLastValueColumn信息，
+		// log_id和数据库对应的字段一致,就不需要设置setLastValueColumn信息，
 		// 但是需要设置setLastValueType告诉工具增量字段的类型
 		/**
 		 * es相关配置
@@ -120,8 +120,7 @@ public class ES2ESScrollAllTimestampDemo {
 		});
 //		//设置任务执行拦截器结束，可以添加多个
 		//增量配置开始
-////		importBuilder.setNumberLastValueColumn("logId");//指定数字增量查询字段变量名称
-//		importBuilder.setDateLastValueColumn("logOpertime");//手动指定日期增量查询字段变量名称
+////		importBuilder.setLastValueColumn("logId");//指定数字增量查询字段变量名称
 //		importBuilder.setFromFirst(true);//任务重启时，重新开始采集数据，true 重新开始，false不重新开始，适合于每次全量导入数据的情况，如果是全量导入，可以先删除原来的索引数据
 //		importBuilder.setLastValueStorePath("es2esdemo_import");//记录上次采集的增量字段值的文件路径，作为下次增量（或者重启后）采集数据的起点，不同的任务这个路径要不一样
 		/**
